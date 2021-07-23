12b8037475a6e51c7086d3a9a411deed2fc0a5a3
Code Lin <code.lin@mediatek.com>
2021-07-24 01:48:27 +0800

    Handle version 2 of TransposeConv
    Update the verification to version 2 for INT8 TransposeConv.

--- neuron/neuron_delegate_validation.cc
+++ neuron/neuron_delegate_validation.cc
@@ -403,7 +403,7 @@ bool Validate(const TfLiteRegistration* registration, const TfLiteNode* node,
       ExpectOpVersion(version, 1, &val_ctx);
     } break;
     case kTfLiteBuiltinTransposeConv: {
-      ExpectOpVersion(version, 1, &val_ctx);
+      ExpectMaxOpVersion(version, 2, &val_ctx);
       Expect((node->inputs->size > 1) &&
                  (context->tensors[node->inputs->data[0]].allocation_type ==
                   kTfLiteMmapRo) &&
