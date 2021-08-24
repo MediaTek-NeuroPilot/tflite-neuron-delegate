266f6f0889af2292e4c718c000d16b6f6f54dd60
Koan-Sin Tan <koansin.tan@gmail.com>
2021-08-24 16:34:12 +0800

    Support new APUWareUtils
    There is new version of APUWareUtils in Android 12.

--- neuron/APUWareUtilsApi.h
+++ neuron/APUWareUtilsApi.h
@@ -1,30 +1,29 @@
 /*
-* Copyright (C) 2021 MediaTek Inc., this file is modified on 02/26/2021
-* by MediaTek Inc. based on MIT License .
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the ""Software""), to
-* deal in the Software without restriction, including without limitation the
-* rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-* sell copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*/
+ * Copyright (C) 2021 MediaTek Inc., this file is modified on 02/26/2021
+ * by MediaTek Inc. based on MIT License .
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the ""Software""), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #pragma once
 
 #include <android/log.h>
 #include <dlfcn.h>
-#include <cstdlib>
 
 #include <cstdlib>
 #include <memory>
@@ -39,8 +38,6 @@ typedef enum {
   FAST_COMPILE_MODE,        // For model compile preference
   PERFORMANCE_MODE_MAX,
 } PERFORMANCE_MODE_E;
-
-#define ABORT_ON_DLOPEN_ERROR
 
 //------------------------------------- -------------------------------------
 #define APUWARE_LOG_D(format, ...)                                  \
@@ -86,7 +83,12 @@ inline void* loadApuWareUtilsLibrary(const char* name) {
 
 inline void* getApuWareUtilsLibraryHandle() {
   if (sAPUWareUtilsLibHandle == nullptr) {
-    sAPUWareUtilsLibHandle = loadApuWareUtilsLibrary("libapuwareutils.mtk.so");
+    sAPUWareUtilsLibHandle =
+        loadApuWareUtilsLibrary("libapuwareutils_v2.mtk.so");
+    if (sAPUWareUtilsLibHandle == nullptr) {
+      sAPUWareUtilsLibHandle =
+          loadApuWareUtilsLibrary("libapuwareutils.mtk.so");
+    }
   }
   return sAPUWareUtilsLibHandle;
 }
